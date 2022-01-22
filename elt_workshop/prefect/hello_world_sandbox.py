94e68c99c75391f8149adb937c1d0d453cbd205f
Sylwester Rąpała <sylwesterrapala@outlook.com>
2022-01-22 17:47:07 +0100

    Add example sandboxed file

---
+++ elt_workshop/prefect/hello_world_sandbox.py
@@ -0,0 +1,18 @@
+from prefect import Flow, task
+from prefect.run_configs import DockerRun
+from prefect.storage import GitHub
+
+ELT_WORKSHOP_REPO = "xoac/elt_workshop"
+STORAGE = GitHub(
+    repo=ELT_WORKSHOP_REPO, path="elt_workshop/prefect/hello_world_sbx.py", ref="sbx"
+)
+
+@task
+def echo(text: str) -> str:
+    return text
+
+with Flow(
+    "Hello, world - sandbox", storage=STORAGE, run_config=DockerRun(labels=["sbx"])
+) as flow:
+    hello_world = echo("Hello, world from cloud!!! 438729")
+
